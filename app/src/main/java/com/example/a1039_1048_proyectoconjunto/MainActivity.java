61c84dd58325498f13b80562220679df9925fbfa
digise <diegogisbertsegura.dg@gmail.com>
2021-11-06 13:32:17 +0100

    Empezamos el proyecto

--- app/src/main/java/com/example/a1039_1048_proyectoconjunto/MainActivity.java
+++ app/src/main/java/com/example/a1039_1048_proyectoconjunto/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hola");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
