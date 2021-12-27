2de43c6e5bbec23051b2cb974087b312e4a4519d
digise <diegogisbertsegura.dg@gmail.com>
2021-12-27 22:36:31 +0100

    Descartamos volley

---
+++ app/src/androidTest/java/com/example/a1039_1048_proyectoconjunto/aceptacion/R1/Historia4Test.java
@@ -0,0 +1,71 @@
+package com.example.a1039_1048_proyectoconjunto.aceptacion.R1;
+/*
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.example.a1039_1048_proyectoconjunto.gestores.Gestor;
+import com.example.a1039_1048_proyectoconjunto.gestores.GestorServicios;
+import com.example.a1039_1048_proyectoconjunto.gestores.GestorUbicaciones;
+import com.example.a1039_1048_proyectoconjunto.servicios.ServicioOpenWeather;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+ */
+
+public class Historia4Test {
+/*
+    private ServicioOpenWeather servicioOpenWeather;
+
+    @BeforeEach
+    void setUp(){
+        servicioOpenWeather = new ServicioOpenWeather();
+    }
+
+    @Test
+    public void validarCoordenadas_APIOpenWeather_coordenadasValidas(){
+        //Given
+        GestorServicios gestorServicios = GestorServicios.getInstance();
+        gestorServicios.addServicio("Open Weather", servicioOpenWeather);
+
+        GestorUbicaciones gestorUbicaciones = GestorUbicaciones.getInstance();
+        Gestor gestor = new Gestor(gestorUbicaciones, gestorServicios);
+
+        double latitud = 39.987889;
+        double longitud = 0.055778;
+
+
+        //When
+        boolean valido = gestor.coordenadasValidas(servicioOpenWeather, latitud, longitud);
+
+
+        //Then
+        //TODO: COMPROBAR QUE LA UBICACIÓN DE ESA LATITUD Y LONGITUD ES LA QUE SE ESPERA
+        assertTrue(valido);
+        assertTrue(servicioOpenWeather.isValid(latitud, longitud));
+    }
+
+    @Test
+    public void validarCoordenadas_APIOpenWeather_coordenadasNoValidas(){
+        //Given
+        GestorServicios gestorServicios = GestorServicios.getInstance();
+        gestorServicios.addServicio("Open Weather", servicioOpenWeather);
+
+        GestorUbicaciones gestorUbicaciones = GestorUbicaciones.getInstance();
+        Gestor gestor = new Gestor(gestorUbicaciones, gestorServicios);
+
+        double latitud = -100;
+        double longitud = -100;
+
+
+        //When
+        boolean valido = gestor.coordenadasValidas(servicioOpenWeather, latitud, longitud);
+
+
+        //Then
+        assertFalse(valido);
+        assertFalse(servicioOpenWeather.isValid(latitud, longitud));
+    }
+
+ */
+}
---
+++ app/src/main/java/com/example/a1039_1048_proyectoconjunto/servicios/ServicioGeocodingPrueba.java
@@ -0,0 +1,39 @@
+package com.example.a1039_1048_proyectoconjunto.servicios;
+
+import com.example.a1039_1048_proyectoconjunto.Ubicacion;
+import com.example.a1039_1048_proyectoconjunto.adapter.GeocodingAdapter;
+
+public class ServicioGeocodingPrueba {
+
+    private final String url = "https://geocode.xyz/";
+    private final String auth = "57673066339488579050x115589";
+
+    public ServicioGeocodingPrueba() {
+
+    }
+
+    public Ubicacion getInformacion(String tipo, String toponimoCoords) {
+        if (tipo.equals("toponimo")) {
+            return getUbicacionByToponimo(toponimoCoords);
+        } else {
+            return getUbicacionByCoordenadas(toponimoCoords);
+        }
+    }
+
+
+    private Ubicacion getUbicacionByToponimo(String toponimoCoords) {
+        String tempUrl = url + toponimoCoords;
+        tempUrl += "?json=1&auth=" + auth;
+        GeocodingAdapter geocodingAdapter = new GeocodingAdapter();
+        return geocodingAdapter.doRequest(tempUrl);
+    }
+
+    private Ubicacion getUbicacionByCoordenadas(String toponimoCoords) {
+        String tempUrl = url + toponimoCoords;
+        tempUrl += "?geoit=json&auth=" + auth;
+        GeocodingAdapter geocodingAdapter = new GeocodingAdapter();
+        return geocodingAdapter.doRequest(tempUrl);
+    }
+
+
+}
