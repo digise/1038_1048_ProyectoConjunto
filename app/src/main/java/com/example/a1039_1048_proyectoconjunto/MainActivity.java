1d0870f5220878c16aaa84d7a8981a6bc9ea9c93
JoanPadilla <joan.padilla2000@gmail.com>
2022-01-04 23:21:11 +0100

    Acabar y corregir el layout customizado

--- app/src/main/java/com/example/a1039_1048_proyectoconjunto/MainActivity.java
+++ app/src/main/java/com/example/a1039_1048_proyectoconjunto/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("hola");
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         startActivity(new Intent(this, GeocodingActivity.class));
     }
 }
