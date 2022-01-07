ade8da43aae70497d4767c4160bccd6b039d3187
JoanPadilla <joan.padilla2000@gmail.com>
2022-01-07 21:57:23 +0100

    Tocando visualmente

--- app/src/main/java/com/example/a1039_1048_proyectoconjunto/activities/ActividadDarAlta.java
+++ app/src/main/java/com/example/a1039_1048_proyectoconjunto/activities/ActividadDarAlta.java
@@ -7,12 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.a1039_1048_proyectoconjunto.R;
+import com.example.a1039_1048_proyectoconjunto.Ubicacion;
 import com.example.a1039_1048_proyectoconjunto.activities.AllUbicacionesActividad;
+import com.example.a1039_1048_proyectoconjunto.gestores.Gestor;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ActividadDarAlta extends AppCompatActivity {
+
+    private Gestor gestor;
 
     private TextInputLayout toponimo;
     private TextInputLayout latitud;
@@ -22,6 +27,8 @@ public class ActividadDarAlta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dar_alta);
+
+        gestor = Gestor.getInstance();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_baseline_arrow_back_24_white));
@@ -40,6 +47,10 @@ public class ActividadDarAlta extends AppCompatActivity {
         botonBuscarToponimo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Ubicacion ubicacion = gestor.getUbicacionPorToponimo(toponimo.getEditText().getText().toString());
+                gestor.darAltaUbicacion(ubicacion);
+                lanzarToast();
+                finish();
             }
         });
 
@@ -48,9 +59,20 @@ public class ActividadDarAlta extends AppCompatActivity {
         botonBuscarCoordenadas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String strLatitud = latitud.getEditText().getText().toString();
+                String strLongitud = longitud.getEditText().getText().toString();
+                Ubicacion ubicacion = gestor.getUbicacionPorCoordenadas(strLatitud, strLongitud);
+                gestor.darAltaUbicacion(ubicacion);
+                lanzarToast();
+                finish();
             }
         });
+    }
+
+
+    private void lanzarToast() {
+        Toast toast = Toast.makeText(getBaseContext(), "Ubicacion encontrada", Toast.LENGTH_LONG);
+        toast.show();
 
     }
 }
