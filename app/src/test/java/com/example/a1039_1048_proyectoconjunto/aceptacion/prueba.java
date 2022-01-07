7af1e31bcfd8719a036ffbc5b731460247cb9637
digise <diegogisbertsegura.dg@gmail.com>
2022-01-07 19:30:18 +0100

    .

---
+++ app/src/test/java/com/example/a1039_1048_proyectoconjunto/aceptacion/prueba.java
@@ -0,0 +1,31 @@
+package com.example.a1039_1048_proyectoconjunto.aceptacion;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.example.a1039_1048_proyectoconjunto.Ubicacion;
+import com.example.a1039_1048_proyectoconjunto.gestores.Gestor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class prueba {
+    private Gestor gestor;
+
+    @BeforeEach
+    public void crear_gestor() {
+        gestor = Gestor.getInstance();
+        gestor.recuperarTodaLaInformacionDeLaAplicacion();
+
+    }
+
+    @Test
+    public void altaUbicacion_toponimoExistente_anadir() {
+        gestor.darAltaUbicacion(gestor.getUbicacionPorToponimo("Calig"));
+        gestor.darAltaUbicacion(gestor.getUbicacionPorToponimo("Valencia"));
+        gestor.darAltaUbicacion(gestor.getUbicacionPorToponimo("Castellon"));
+        gestor.darAltaUbicacion(gestor.getUbicacionPorToponimo("Alicante"));
+    }
+
+}
